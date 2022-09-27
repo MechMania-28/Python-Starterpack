f1ac393d3e1b638b40efeda76f15796dd2f01f79
hanby <boyouhan@gmail.com>
2022-09-27 01:02:35 -0500

    updated build.py

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
 import zipapp
-print("Writing to out.pyz...")
-zipapp.create_archive('.', main='main:main', target='bot')
+print("Writing to bot.pyz...")
+zipapp.create_archive('.', main='main:main', target='bot.pyz')
 print("Done!")
