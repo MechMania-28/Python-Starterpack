8af00dade1089e4faf2567318e2464fa1c096e7f
hanby <boyouhan@gmail.com>
2022-09-28 00:52:28 -0500

    updated strategy config

--- strategy/strategy_config.py
+++ strategy/strategy_config.py
@@ -1,5 +1,6 @@
 from strategy.new_strategy import NewStrategy
 from strategy.starter_strategy import StarterStrategy
+from strategy.strategy import Strategy
 
-def get_strategy(player_index: int):
+def get_strategy(player_index: int) -> Strategy:
   return StarterStrategy()
