ab22859d0e07eaa4c463e9990261c613b63f50d0
hanby <boyouhan@gmail.com>
2022-09-28 14:59:22 -0500

    refactored util

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
-class Config:
-    board_size = 10
-    max_score = 100
-    turns = 50
-    gold_per_turn = 1
-    ports = [29170, 29171, 29172, 29173]
+BOARD_SIZE = 10
+MAX_SCORE = 100
+TURNS = 50
+GOLD_PER_TURN = 1
+PORTS = [29170, 29171, 29172, 29173]
--- util/utility.py
+++ util/utility.py
@@ -1,35 +1,21 @@
+from enum import Enum
 import math
 import random
+from typing import Any, Type
 
 import config
 
-import player.position
+from player.position import Position
 
-class Utility(object):
-    """ generated source for class Utility """
-    @classmethod
-    def manhattanDistance(cls, p1, p2):
-        """ generated source for method manhattanDistance """
-        return math.abs(p1.getX() - p2.getX()) + math.abs(p1.getY() - p2.getY())
+def manhattan_distance(p1: Position, p2: Position) -> int:
+    return math.abs(p1.getX() - p2.getX()) + math.abs(p1.getY() - p2.getY())
 
-    @classmethod
-    def squareDistance(cls, p1, p2):
-        """ generated source for method squareDistance """
-        return math.max(math.abs(p1.getX() - p2.getX()), math.abs(p1.getY() - p2.getY()))
+def chebyshev_distance(p1: Position, p2: Position) -> int:
+    return math.max(math.abs(p1.getX() - p2.getX()), math.abs(p1.getY() - p2.getY()))
 
-    @classmethod
-    def randomInt(cls, min, max):
-        """ generated source for method randomInt """
-        return random.randint(min, max + 1)
+def in_bounds(p: Position) -> bool:
+    #  Assume board runs from 0 to BOARD_SIZE - 1
+    return ((p.getX() >= 0) and (p.getX() < config.BOARD_SIZE) and (p.getY() >= 0) and (p.getY() < config.BOARD_SIZE))
 
-    @classmethod
-    def inBounds(cls, p):
-        """ generated source for method inBounds """
-        #  Assume board runs from 0 to BOARD_SIZE - 1
-        return ((p.getX() >= 0) and (p.getX() < config.BOARD_SIZE) and (p.getY() >= 0) and (p.getY() < config.BOARD_SIZE))
-
-    @classmethod
-    def randomEnum(cls, clazz):
-        """ generated source for method randomEnum """
-        x = random.nextInt(clazz.getEnumConstants().length)
-        return clazz.getEnumConstants()[x]
+def random_enum(clazz: Enum):
+    return random.choice(list(clazz))
