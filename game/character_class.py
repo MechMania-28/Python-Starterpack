69e797e409c4850a46680576c0d0c7b6d470041d
Han <boyouh2@illinois.edu>
2022-10-01 14:22:37 -0500

    updated new statset for wizard and archer

--- game/character_class.py
+++ game/character_class.py
@@ -5,5 +5,5 @@ from game.stat_set import StatSet
 
 class CharacterClass(Enum):
   KNIGHT = StatSet(9, 6, 2, 1)
-  WIZARD = StatSet(6, 4, 3, 2)
-  ARCHER = StatSet(3, 2, 4, 3)
+  WIZARD = StatSet(6, 5, 3, 2)
+  ARCHER = StatSet(3, 3, 4, 3)
